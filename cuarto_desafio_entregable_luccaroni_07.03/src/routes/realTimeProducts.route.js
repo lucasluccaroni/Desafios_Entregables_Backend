39ba36c082aa535c11a906bf6542061a7860e70d
LUCAS JOSE LUCCARONI <lucas.luccaroni@vicentelopez.gov.ar>
2024-03-14 10:24:01 -0300

    configurando web socket

--- cuarto_desafio_entregable_luccaroni_07.03/src/routes/realTimeProducts.route.js
+++ cuarto_desafio_entregable_luccaroni_07.03/src/routes/realTimeProducts.route.js
@@ -24,6 +24,7 @@ router.get("/", async(_, res) =>{
         res.render("realTimeProducts", {
             title: "Real Time Products",
             products: productsData,
+            useWs: true,
             styles: ["realTimeProducts.css"]
 
         })
